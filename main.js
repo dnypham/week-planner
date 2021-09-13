6db2d97f2848e7a94eacbfdfa53d2e2c34a3202f
Derek-Cruz <Derekcruz94@gmail.com>
2021-09-13 15:30:30 -0700

    Completed Step 1 onto step 2.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var $form = document.querySelector('form');
 var $popup = document.querySelector('.pop-up-container');
+var $addEntryButton = document.querySelector('#add-entry-button');
+
+$addEntryButton.addEventListener('click', function (event) {
+  $popup.classList.remove('hidden');
+});
 
 $form.addEventListener('click', function (event) {
-  if (event.target.matches('#add-entry-button')) {
-    $popup.classList.remove('hidden');
+  if (event.target.matches('.submit-button')) {
+    event.preventDefault();
+    $popup.classList.add('hidden');
   }
 });
