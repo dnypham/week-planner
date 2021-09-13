e709ee903c36528d8930e73a41abb9de5f036f51
Daniel Pham <dnypham@gmail.com>
2021-09-13 15:15:53 -0700

    Working on Add Entry modal JS.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 var $form = document.querySelector('form');
+var $popup = document.querySelector('.pop-up-container');
 
 $form.addEventListener('click', function (event) {
-
+  if (event.target.matches('#add-entry-button')) {
+    $popup.classList.remove('hidden');
+  }
 });
